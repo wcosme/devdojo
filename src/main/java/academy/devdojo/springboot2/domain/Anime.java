84f4f70bd13c86fce31125c77752775c706b8cbb
Wallace Gonçalves <wallacecosme@gmail.com>
2021-11-07 18:44:36 -0300

    Banco em memoria com H2

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "TB_ANIME")
 public class Anime {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
